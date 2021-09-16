d15a3b0918b1be0624975eeaaad98dd24d0dcae0
Darsonn <87154431+Darsonn@users.noreply.github.com>
2021-09-16 22:17:49 +0200

    Update

--- src/LoadingScreen.java
+++ src/LoadingScreen.java
@@ -1,2 +1,50 @@
-public class LoadingScreen {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class LoadingScreen extends JFrame implements KeyListener {
+    private JLabel loadingLabel;
+
+    public LoadingScreen() {
+        super("Loading");
+
+        loadingLabel = new JLabel("Ladowanie");
+        loadingLabel.setFont(new Font("", Font.BOLD, 70));
+
+        setLayout(new GridBagLayout());
+        getContentPane().setBackground(new Color(0,0,0));
+
+        addKeyListener(this);
+        
+        add(loadingLabel, BoxLayout.X_AXIS);
+
+//        setAlwaysOnTop(true);
+//        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        pack();
+        setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent evt) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent evt) {
+        if (evt.getKeyCode() == 113)
+            biosRun();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent evt) {
+
+    }
+
+    public static void biosRun() {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 /*
-TODO: 1. Ladowanie 2. Powitanie pierwszego uruchomienia
+TODO: 1. Ladowanie 2. Powitanie pierwszego uruchomienia 3. BIOS
  */
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoadingScreen();
+            }
+        });
     }
 }
